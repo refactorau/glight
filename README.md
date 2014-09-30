80b305f5bb7b239faf29c4595f7808b1c504acb8
spidie <steve@refactor.com.au>
2014-10-01 07:21:47 +1000

    now initialising correctly

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ glight
 
 Groovy based build light controller. In true minimalist fashion this script only works with Delcom 904006 Build Light and Jenkins and is tested on Ubunut 14.04. Please fork and adapt if you find it useful.
 
-	sudo apt-get install libhidapi-dev
+	sudo apt-get install libhidapi libusb-java
--- src/main/groovy/au/com/refactor/BuildWatcher.groovy
+++ src/main/groovy/au/com/refactor/BuildWatcher.groovy
@@ -1,12 +1,16 @@
 package au.com.refactor
 
+import com.codeminders.hidapi.ClassPathLibraryLoader
 import com.codeminders.hidapi.HIDDevice
 import com.codeminders.hidapi.HIDManager
 
 class BuildWatcher {
 
     public static void main(String[] args) {
+        ClassPathLibraryLoader.loadNativeHIDLibrary()
         HIDManager hidManager = HIDManager.getInstance();
-        HIDDevice dev = hidManager.openById(0x0fc5, 0xb08, null)
+        println hidManager.listDevices()
+        HIDDevice dev = hidManager.openById(4037, 45184, null)
+        dev.
     }
 }
